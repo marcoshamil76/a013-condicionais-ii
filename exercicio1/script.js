5f29ab8ed3dbf7598ab2a858d8c1ea3d646c8f2b
marcoshamil76 <marcoshamilton1976@gmail.com>
2022-09-08 13:49:48 -0300

    Entrega Exercicio 01

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,23 @@
-const num = //seu numero aqui
 
+const numDigitado = Number(prompt("Digite um número"))
+console.log(numDigitado)
+
+// if (numDigitado % 2 === 0) {
+//     console.log("Número divisível por 2")
+
+//   } else
+//     console.log("Número não divisível por 2")
+
+// if (numDigitado % 3 === 0) {
+//     console.log("Número divisível por 3")
+// }else
+//     console.log("Número não divisível por 3")
+
+
+    if (numDigitado % 2 === 0 && !numDigitado % 3 === 0){
+        console.log ("Número divisível por 2")
+
+    }else if (!numDigitado % 2 === 0 && numDigitado % 3 === 0){
+        console.log ("Número divisível por 3")
+    }else
+        console.log ("Número não divisível por 2 e 3")
